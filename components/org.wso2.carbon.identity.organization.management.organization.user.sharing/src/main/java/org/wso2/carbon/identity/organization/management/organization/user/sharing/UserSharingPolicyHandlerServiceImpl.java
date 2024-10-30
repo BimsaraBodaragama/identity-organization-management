0c1128076eb3c5dd6324c5016e5402209bf70d79
BimsaraBodaragama <bodaragamadb.office@gmail.com>
2024-10-30 20:31:13 +0530

    Initiated user sharing both selective and general

--- components/org.wso2.carbon.identity.organization.management.organization.user.sharing/src/main/java/org/wso2/carbon/identity/organization/management/organization/user/sharing/UserSharingPolicyHandlerServiceImpl.java
+++ components/org.wso2.carbon.identity.organization.management.organization.user.sharing/src/main/java/org/wso2/carbon/identity/organization/management/organization/user/sharing/UserSharingPolicyHandlerServiceImpl.java
@@ -20,10 +20,16 @@ package org.wso2.carbon.identity.organization.management.organization.user.shari
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.UserShareGeneral;
 import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.UserShareGeneralDO;
+import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.UserShareSelective;
 import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.UserShareSelectiveDO;
 import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.UserUnshareGeneralDO;
 import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.UserUnshareSelectiveDO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Service implementation for handling user sharing policies.
@@ -32,38 +38,128 @@ public class UserSharingPolicyHandlerServiceImpl implements UserSharingPolicyHan
 
     private static final Log LOG = LogFactory.getLog(UserSharingPolicyHandlerServiceImpl.class);
 
+    /**
+     * Propagates the selective share of a user to specific organizations.
+     *
+     * @param userShareSelectiveDO Contains details for selective sharing.
+     */
     @Override
     public void propagateSelectiveShare(UserShareSelectiveDO userShareSelectiveDO) {
 
-        LOG.info("Came in to propagateSelectiveShare");
+        List<String> userIds = userShareSelectiveDO.getUserCriteria().get("userIds");
+        List<Map<String, Object>> organizations = userShareSelectiveDO.getOrganizations();
 
-        validateUserShareSelectiveDO(userShareSelectiveDO);
-        //Create a shared user object for the given org - use current user association method ?What's this domain story?
-        //Then assign each role - use current role assignment method
-        //    - get the role id from the given (role name + audience name + audience type)
-        //Then check the policy
-        //    - If policy comes with a code type for future create a UserSharingPolicyRecord and save to DB
+        for (String userId : userIds) {
+            for (Map<String, Object> orgDetails : organizations) {
+                UserShareSelective userShareSelective = new UserShareSelective();
+                userShareSelective.setUserId(userId);
+                userShareSelective.setOrganizationId((String) orgDetails.get("orgId"));
+                userShareSelective.setPolicy((String) orgDetails.get("policy"));
 
-        //?? do we have to validate the data on the payload by checking if the user exists in the db by sending a
-        // db call and check the roles in the db  and check if the policy is a valid policy??
+                List<String> roleIds = extractRoleIds(orgDetails.get("roles"));
+                userShareSelective.setRoles(roleIds);
 
-        LOG.info("Went out from propagateSelectiveShare");
-
+                shareUserWithOrganization(userShareSelective);
+            }
+        }
     }
 
+    /**
+     * Propagates the general share of a user to all organizations.
+     *
+     * @param userShareGeneralDO Contains details for general sharing.
+     */
     @Override
     public void propagateGeneralShare(UserShareGeneralDO userShareGeneralDO) {
 
+        List<String> userIds = userShareGeneralDO.getUserCriteria().get("userIds");
+        String policy = userShareGeneralDO.getPolicy();
+        List<String> roleIds = getRoleIds(userShareGeneralDO.getRoles());
+
+        for (String userId : userIds) {
+            UserShareGeneral userShareGeneral = new UserShareGeneral();
+            userShareGeneral.setUserId(userId);
+            userShareGeneral.setPolicy(policy);
+            userShareGeneral.setRoles(roleIds);
+
+            shareUserWithAllOrganizations(userShareGeneral);
+        }
     }
 
     @Override
     public void propagateSelectiveUnshare(UserUnshareSelectiveDO userUnshareSelectiveDO) {
-
+        // TODO: To be implemented on selective unsharing
     }
 
     @Override
     public void propagateGeneralUnshare(UserUnshareGeneralDO userUnshareGeneralDO) {
+        // TODO: To be implemented on general unsharing
+    }
 
+    /**
+     * Handles storing or processing the user-organization share.
+     *
+     * @param userShareSelective Contains details for sharing a user with an organization.
+     */
+    private void shareUserWithOrganization(UserShareSelective userShareSelective) {
+
+        //In this method we create an UserAssociation model and share the user
+        //We do the role assign here sa well
+
+    }
+
+    /**
+     * Handles storing or processing the general user share.
+     *
+     * @param userShareGeneral Contains details for sharing a user with all organizations.
+     */
+    private void shareUserWithAllOrganizations(UserShareGeneral userShareGeneral) {
+
+        //We get all orgs under the policy given by userShareGeneral.
+        //We can use private List<String> getOrganizationsBasedOnPolicy(String policy) for that
+        //Then we iterate through those orgs and create an UserAssociation model and share the user
+        //Then inside that loop, we do the role assign for each inside that
+
+    }
+
+    /**
+     * Converts roles to role IDs.
+     *
+     * @param roles The list of roles containing display name and audience details.
+     * @return The list of role IDs.
+     */
+    private List<String> getRoleIds(List<Map<String, String>> roles) {
+        List<String> roleIds = new ArrayList<>();
+
+        //We have to get the role name, audience name and type and get the role id from the db and return the
+        // roleId list
+
+        return roleIds;
+    }
+
+    /**
+     * Extracts role IDs from the given roles object.
+     *
+     * @param rolesObj The roles object to be processed.
+     * @return A list of role IDs.
+     */
+    private List<String> extractRoleIds(Object rolesObj) {
+        List<String> roleIds = new ArrayList<>();
+
+        if (rolesObj instanceof List<?>) {
+            List<?> rolesList = (List<?>) rolesObj;
+            if (!rolesList.isEmpty() && rolesList.get(0) instanceof Map) {
+                try {
+                    @SuppressWarnings("unchecked")
+                    List<Map<String, String>> castedRolesList = (List<Map<String, String>>) rolesList;
+                    roleIds = getRoleIds(castedRolesList);
+                } catch (ClassCastException e) {
+                    throw new IllegalArgumentException("Invalid roles format", e);
+                }
+            }
+        }
+
+        return roleIds;
     }
 
     private void validateUserShareSelectiveDO(UserShareSelectiveDO userShareSelectiveDO) {

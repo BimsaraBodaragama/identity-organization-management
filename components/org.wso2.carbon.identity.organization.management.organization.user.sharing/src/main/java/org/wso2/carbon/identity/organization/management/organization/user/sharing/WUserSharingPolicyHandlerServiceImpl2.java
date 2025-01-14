c350ec1b61cb882de6b1fa05c9379b50cd56890c
BimsaraBodaragama <bodaragamadb.office@gmail.com>
2025-01-14 20:35:26 +0530

    cmp_user-share: Restructure models and check api calls

--- components/org.wso2.carbon.identity.organization.management.organization.user.sharing/src/main/java/org/wso2/carbon/identity/organization/management/organization/user/sharing/WUserSharingPolicyHandlerServiceImpl2.java
+++ components/org.wso2.carbon.identity.organization.management.organization.user.sharing/src/main/java/org/wso2/carbon/identity/organization/management/organization/user/sharing/WUserSharingPolicyHandlerServiceImpl2.java
@@ -14,7 +14,10 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+<<<<<<< HEAD
  */
+=======
+>>>>>>> d0950132 (cmp_user-share: Restructure models and check api calls)
 
 
 

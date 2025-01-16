a51fc094bc41042d09570030af001ddef6fe1265
BimsaraBodaragama <bodaragamadb.office@gmail.com>
2025-01-17 01:59:44 +0530

    cmp_user-share: Implement unshare.

--- components/org.wso2.carbon.identity.organization.management.organization.user.sharing/src/main/java/org/wso2/carbon/identity/organization/management/organization/user/sharing/exception/UserShareMgtException.java
+++ components/org.wso2.carbon.identity.organization.management.organization.user.sharing/src/main/java/org/wso2/carbon/identity/organization/management/organization/user/sharing/exception/UserShareMgtException.java
@@ -26,35 +26,22 @@ public class UserShareMgtException extends Exception {
     private final String errorCode;
     private final String description;
 
-    public UserShareMgtException(String message, String errorCode, String description) {
+    public UserShareMgtException(String errorCode, String message, String description) {
 
         super(message);
         this.errorCode = errorCode;
         this.description = description;
     }
 
-    public UserShareMgtException(String errorCode, String description) {
-
-        this.errorCode = errorCode;
-        this.description = description;
-    }
-
-    public UserShareMgtException(String message, Throwable cause, String errorCode, String description) {
+    public UserShareMgtException(String errorCode, String message, String description, Throwable cause) {
 
         super(message, cause);
         this.errorCode = errorCode;
         this.description = description;
     }
 
-    public UserShareMgtException(Throwable cause, String errorCode, String description) {
-
-        super(cause);
-        this.errorCode = errorCode;
-        this.description = description;
-    }
-
-    public UserShareMgtException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace,
-                                 String errorCode, String description) {
+    public UserShareMgtException(String errorCode, String message, String description, Throwable cause,
+                                 boolean enableSuppression, boolean writableStackTrace) {
 
         super(message, cause, enableSuppression, writableStackTrace);
         this.errorCode = errorCode;
